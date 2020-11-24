f871af7cd13655687721b834b67bd99de85f9129
vitShum <56528651+vitShum@users.noreply.github.com>
2020-11-24 12:57:15 +1000

    Add files via upload

---
+++ WebApplication1/WebApplication1/Models/MysqlDbContext.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApplication1.Models
+{
+    public partial class MysqlDbContext : DbContext
+    {
+        public MysqlDbContext()
+        {
+        }
+
+        public MysqlDbContext(DbContextOptions<MysqlDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Departments> Departments { get; set; }
+        public virtual DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySql("server=localhost;port=3306;user=root;password=root;database=1141", x => x.ServerVersion("10.5.8-mariadb"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Departments>(entity =>
+            {
+                entity.ToTable("departments");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasColumnType("varchar(50)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+            });
+
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.ToTable("users");
+
+                entity.HasIndex(e => e.IdDepartment)
+                    .HasName("FK_users_id_department");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Fname)
+                    .HasColumnName("fname")
+                    .HasColumnType("varchar(255)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+
+                entity.Property(e => e.IdDepartment)
+                    .HasColumnName("id_department")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Sname)
+                    .HasColumnName("sname")
+                    .HasColumnType("varchar(255)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+
+                entity.HasOne(d => d.IdDepartmentNavigation)
+                    .WithMany(p => p.Users)
+                    .HasForeignKey(d => d.IdDepartment)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_users_id_department");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models
+{
+    public partial class Users
+    {
+        public int Id { get; set; }
+        public string Fname { get; set; }
+        public string Sname { get; set; }
+        public int IdDepartment { get; set; }
+
+        public virtual Departments IdDepartmentNavigation { get; set; }
+    }
+}
